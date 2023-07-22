8efd1b7a164ad8f8cf07460d6050b1e1ca5715d4
n8jhj <nathaniel-j-jones@proton.me>
2023-07-22 16:42:53 -0700

    Add compile script and allow quit with Q key
    Also added .gitignore for compiled executables.

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,13 @@ int main()
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q))
+            {
+                window.close();
+            }
         }
 
         window.clear();
